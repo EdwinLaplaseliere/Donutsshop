5e462be1c6168914b0a116259aab8e15a4b78f4b
Edwingar <garces_92@hotmail.com>
2019-12-08 21:46:55 +0000

    Donuts page updated

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var http = require('http'), //This module provides the HTTP server functionaliti
     xmlParse = require('xslt-processor').xmlParse, //This module allows us to work with XML files
     xsltProcess = require('xslt-processor').xsltProcess, //The same module allows us to utilise XSL Transformations
     xml2js = require('xml2js'); //This module does XML to JSON conversion and also allows us to get from JSON back to XML
-
+require ('views/js');
 var router = express(); //The set our routing to be handled by Express
 var server = http.createServer(router); //This is where our server gets created
 
