614c1539a85364a63340cc4e66db1b302b98ecce
Edwingar <garces_92@hotmail.com>
2019-12-08 22:28:03 +0000

    Donuts page updated

--- views/js/edwinsdonutsCode.js
+++ views/js/edwinsdonutsCode.js
@@ -53,17 +53,17 @@ function getParentTag(oNode, sParentType) {
  
  function validate() {
 
-    var checkitemnamee=document.getElementById("itemname").value;
-    var checkprice=document.getElementById("itemprice").value;
+    var checkitemnamee=document.getElementById("itemname").value; //takes a item name from the index.html
+    var checkprice=document.getElementById("itemprice").value;// takes an item price from the index.html
 
-            if (checkitemnamee== "") {
-                alert("A name should be included");
+            if (checkitemnamee== "") {// verifies that there is text into the input 
+                alert("A name should be included");// an alert if there is not text inputed
             }else if(checkprice ==""){
-                alert("Please enter a price");
+                alert("Please enter a price");// an alert if there is not numbers inputed
 
-            } else if (/^[A-Za-z ]+$/.test(checkitemnamee) ){
+            } else if (/^[A-Za-z ]+$/.test(checkitemnamee) ){// verifies a correct name
  
-    if(validate_float()){
+    if(validate_float(checkprice)){// gets a true or false value from the validate_float function
     return true;
 
       }else {
@@ -73,7 +73,7 @@ function getParentTag(oNode, sParentType) {
 
             }
             else{
-            alert("Only text input allowed as item name");
+            alert("Only text input allowed as item name");// if the customer enters a number or a simbol it tell the customer to please enter a valid input
             return false;
             }
 
@@ -83,6 +83,8 @@ function getParentTag(oNode, sParentType) {
         }
 
 
+
+// a function to validate the float numbers for the prices
 function validate_float(checkprice){
 
 if(/^[+-]?\d+(\.\d+)?$/.test(checkprice)){
